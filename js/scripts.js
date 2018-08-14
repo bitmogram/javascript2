759a70195fe7f820922ec9513f0c418247757e76
Maurice <bitmogram@gmail.com>
2018-08-13 20:59:47 -0700

    end of day1 commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+/* A semi colon is also called a delimiter. It separates phrases from each other.They are used at the end of lines of code */
+/* console access the console. the period takes us down a level. the phrase hello world is a string.*/
+
+console.log("Hello World!");
+alert("Hello! I am an alert box!!");
