7572a9dc398292d904268eae83594a9d61f2d4d5
Maurice <bitmogram@gmail.com>
2018-08-15 19:53:24 -0700

    Updated notes file

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,10 @@ var myAge = 35;
 // console.log(myName);
 // console.log(myAge);
 
-// ES5
-console.log("Hello, " + myName + "! You are " + myAge + " years old. ");
+// ES5 way to log out to the console myName and myAge.
+// console.log("Hello, " + myName + "! You are " + myAge + " years old. ");
 
+//ES6 way to log out to the console mName and myAge.
+console.log(`Hello, ${myName}! You are ${myAge} years old.`)
 //Var isn't being used much. Let and Const are used more often.  We will use let more than we use const.
 // const myName = "Maurice";
