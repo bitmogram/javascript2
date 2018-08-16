9804fcabc0ec4897589ae2bee344efdf2754a145
Maurice <bitmogram@gmail.com>
2018-08-15 19:32:11 -0700

    Saving during the break so I don't loose this code I don't need

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// console.log("Hello World!");
+
+//Declare a variable
+var myName = "Maurice";
+var myAge = 35;
+// console.log(myName);
+// console.log(myAge);
+
+// ES5
+console.log("Hello, " + myName + "! You are " + myAge + " years old. ");
+
+//Var isn't being used much. Let and Const are used more often.  We will use let more than we use const.
+// const myName = "Maurice";
